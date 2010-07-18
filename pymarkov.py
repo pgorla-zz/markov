edbec7c49bf9f7dab535faf0b1131bb3a3843810
Patricia <gorla.patricia@gmail.com>
2010-07-18 16:16:42 -0600

    testing data types for table

--- pymarkov.py
+++ pymarkov.py
@@ -1,44 +1,48 @@
 #!/usr/bin/env python
 
-from random import randint
-
+import random
 
 def generate_markov_table(text, look_forward):
 
-    table = []
+    table = {}
 
     # walk through text, make index table
     for i in range(len(text)):
         char = text[i:look_forward]
-        if not char in table:
-            table[char] = []
+        if char not in table:
+            table[char] = {}
+    print table
 
     # walk array again, count numbers
     for i in range(len(text) - look_forward):
         char_index = text[i:look_forward]
         char_count = text[i+look_forward:look_forward]
 
-        if table[char_index][char_count]:
-            table[char_index][char_count] += 1
-        else:
+        if not char_count in table[char_index].keys():
             table[char_index][char_count] = 1
+        else:
+            table[char_index][char_count] += 1
 
     return table
 
 
 def generate_markov_text(length, table, look_forward):
     # get first character
-    char = table[randint(0,len(table))]
+    char = random.choice(table.keys())
     o = char
 
     for i in range(length/look_forward):
+        li_nums = []
+        for k,v in table[char]:
+            li_nums.append(v)
+
         newchar = return_weighted_char(table[char])
 
         if newchar:
             char = newchar
             o = newchar
         else:
-            char = table[randint(0,len(table))]
+            char = random.choice(table.keys())
 
     return o
 
@@ -48,7 +52,7 @@ def return_weighted_char(array):
         return False
     else:
         total = sum(array)
-        rand = randint(1,total)
+        rand = random.randint(1,total)
     for weight in array:
         if rand <= weight:
             return weight
@@ -69,6 +73,12 @@ def return_weighted_char(array):
 
 
 
+if __name__ == '__main__':
+    fin = open('text/alice.txt')
+    text = ''
+    for line in fin:
+        text += line.strip()
+    generate_markov_table(text, 4)
 
 
 
