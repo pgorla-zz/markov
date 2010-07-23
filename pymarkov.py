5f81ed28454c7305b402825d6b3b0576d72d2ec0
Patricia <gorla.patricia@gmail.com>
2010-07-23 17:03:36 -0600

    woo class action!

--- pymarkov.py
+++ pymarkov.py
@@ -1,77 +1,83 @@
 #!/usr/bin/env python
 
+import sys
 import random
+import string
 
-def generate_markov_table(text, look_forward):
+class Markov(object):
 
-    table = {}
+    """
+    Markov-chain text generator. Translated
+    from the original PHP into Python (haykranen.nl)
+    """
 
-    # walk through text, make index table
-    for i in range(len(text)):
-        char = text[i:i+look_forward]
-        if char not in table:
-            table[char] = {}
+    def __init__(self,text='alice.txt',length=600):
+        fil = text
+        fin = open('text/'+fil)
+        self.text = ''
+        forbid = '\\'
+        for line in fin:
+            self.text += line.translate(None,forbid).strip()
+        self.markov_table = self.generate_markov_table(self.text, 3)
+        self.length = length
 
-    # walk array again, count numbers
-    for i in range(len(text) - look_forward):
-        char_index = text[i:i+look_forward]
-        char_count = text[i+look_forward:i+look_forward+look_forward]
 
-        if char_count not in table[char_index].keys():
-            table[char_index][char_count] = 1
+    def _run(self):
+
+        return self.generate_markov_text(self.length,self.markov_table,3)
+
+
+    def generate_markov_table(self, text, look_forward):
+        self.table = {}
+
+        # walk through text, make index table
+        for i in range(len(self.text)):
+            char = self.text[i:i+look_forward]
+            if char not in self.table:
+                self.table[char] = {}
+
+        # walk array again, count numbers
+        for i in range(len(self.text) - look_forward):
+            char_index = self.text[i:i+look_forward]
+            char_count = self.text[i+look_forward:i+look_forward+look_forward]
+
+            if char_count not in self.table[char_index].keys():
+                self.table[char_index][char_count] = 1
+            else:
+                self.table[char_index][char_count] += 1
+
+        return self.table
+
+
+    def generate_markov_text(self, length, table, look_forward):
+        # get first character
+        char = random.choice(table.keys())
+        o = char
+        for i in range(length/look_forward):
+            newchar = self.return_weighted_char(table[char])
+            if newchar:
+                char = newchar
+                o += newchar
+            else:
+                char = random.choice(table.keys())
+        return o
+
+
+    def return_weighted_char(self,array):
+        if not array:
+            return False
         else:
-            table[char_index][char_count] += 1
-
-    return table
-
-
-def generate_markov_text(length, table, look_forward):
-    # get first character
-    char = random.choice(table.keys())
-    o = char
-    for i in range(length/look_forward):
-        newchar = return_weighted_char(table[char])
-        if newchar:
-            char = newchar
-            o += newchar
-        else:
-            char = random.choice(table.keys())
-    return o
-
-
-def return_weighted_char(array):
-    if not array:
-        return False
-    else:
-        total = sum(array.values())
-        rand = random.randint(1,total)
-        for k,v in array.iteritems():
-            if rand <= v:
-                return k
-            rand -= v
+            total = sum(array.values())
+            rand = random.randint(1,total)
+            for k,v in array.iteritems():
+                if rand <= v:
+                    return k
+                rand -= v
 
 
 
 
 
 if __name__ == '__main__':
-    import sys
-    import string
-    if sys.argv[1]:
-        fil = sys.argv[1]
-    else:
-        fil = 'alice.txt'
-    if sys.argv[2]:
-        length = int(sys.argv[2])
-    else:
-        length = 600
-
-    fin = open('text/'+fil)
-    text = ''
-    forbid = '\\'
-    for line in fin:
-        text += line.translate(None,forbid).strip()
-    markov = generate_markov_table(text, 4)
-    print generate_markov_text(length,markov,3)
-
-
+    t = Markov()
+    print t._run()
